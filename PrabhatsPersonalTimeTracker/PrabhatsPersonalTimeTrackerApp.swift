8fcecb113c6405dbb3597ea8af481926700e128a
Pravat Pandey <pandeypravat@gmail.com>
2025-04-21 09:14:12 +0545

    Initial Commit

---
+++ PrabhatsPersonalTimeTracker/PrabhatsPersonalTimeTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrabhatsPersonalTimeTrackerApp.swift
+//  PrabhatsPersonalTimeTracker
+//
+//  Created by Pravat on 21/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct PrabhatsPersonalTimeTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PrabhatsPersonalTimeTrackerTests/PrabhatsPersonalTimeTrackerTests.swift
@@ -0,0 +1,16 @@
+//
+//  PrabhatsPersonalTimeTrackerTests.swift
+//  PrabhatsPersonalTimeTrackerTests
+//
+//  Created by Pravat on 21/04/2025.
+//
+
+import Testing
+
+struct PrabhatsPersonalTimeTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
